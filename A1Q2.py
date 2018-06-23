b4544f33afad0d5b6fdb7679ec31d876aceb3b4f
mustardjelly <32181718+mustardjelly@users.noreply.github.com>
2018-06-23 13:23:24 +1200

    Add process_data function
    Calculates the number of elements from a data set to begin estimating the min elements

--- A1Q2.py
+++ A1Q2.py
@@ -6,6 +6,7 @@
 #            mgu011                             #
 #            4364426                            #
 #################################################
+import math
 
 # Calculates and returns the GCD of two numbers
 def gcd(number1, number2):
@@ -30,3 +31,15 @@ def gcd(number1, number2):
                 return -number2
 				
 data_set = [10, 16, 8, 3, 4, 2, 1, 60, 33, 12, 18, 13]
+
+# Sets processing boundaries to speed up large calculations by setting a limit on
+# the number of elements to look at.
+# Input: a data set
+# Output: (int) a number of elements to process
+def process_data(aDataSet):
+	length = len(aDataSet)
+	if (length > 1000000):
+		return 100000
+	if length > 100000):
+		return length / 2
+		

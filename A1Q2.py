3216133055ba5a4443814c56c460bc5cee527ee6
mustardjelly <32181718+mustardjelly@users.noreply.github.com>
2018-06-25 16:37:52 +1200

    Add Core Functions

--- A1Q2.py
+++ A1Q2.py
@@ -7,39 +7,72 @@
 #            4364426                            #
 #################################################
 import math
+import copy
+import random
 
-# Calculates and returns the GCD of two numbers
-def gcd(number1, number2):
-    remainder = ''
-    quotient = 0
-    
-    # Sets number1 to be the larger number
-    if number2 > number1:
-        tempNumber = number1
-        number1 = number2
-        number2 = tempNumber
-        
-    while (True):#remainder != 0):
-        remainder = number1 % number2
-        if remainder != 0:
-            number1 = number2
-            number2 = remainder
-        else:
-            if number2 > 0:
-                return number2
-            else:
-                return -number2
+input_string = "10 20 50 51 52 53 54 12 15 16 "
+data_set = input_string.split()
+data_set = [random.randint(0,20000) for i in range(20000)]
+
+for i in range(len(data_set)):
+	data_set[i] = int(data_set[i])
+
+def add_to_dict(a_dict, value):
+	try:
+		a_dict[value] += 1
+	except:
+		a_dict[value] = 1
+
+def pairwise_min(data_set):
+	data_length = len(data_set)
+	current_min = {}
+	row_min = []
+	pair_data = []
+	
+	for i in range(data_length):
+		for j in range(data_length - (i)):
+			j = j + i
+			if (i != j):
+				#print("{0}: {2}\n{1}: {3}\n".format(i, j, data_set[i], data_set[j]))
+				temp_min = data_set[i] + data_set[j]
+				row_min += [temp_min]
 				
-data_set = [10, 16, 8, 3, 4, 2, 1, 60, 33, 12, 18, 13]
+	for value in row_min:
+		add_to_dict(current_min, value)
+		
+	for key, value in current_min.items():
+		if (value > 1):
+			# print("{0}: {1}".format(key, value))
+			pair_data += [key]
+	try:
+		return min(pair_data)
+	except:
+		return None
+		
+def sample_min(in_data):
+	data_copy = copy.deepcopy(in_data)
+	data_size = len(in_data)
+	
+	while (data_size > 10000):
+		data_size = int(data_size / 5)
+	data_copy = data_copy[:data_size]
+	local_min = pairwise_min(data_copy)
+	
+	return local_min
+		
+def input(in_data):
+	in_data = list(set(in_data))
+	local_min = sample_min(in_data)
+	
+	filtered_list = [x for x in in_data if x <= local_min]
+	pair_min = pairwise_min(filtered_list)
+	
+	if (not(pair_min)):
+		print("None")
+	else:
+		print("Yes: {0}".format(pair_min))
 		
 def main(in_data):
-	data_set = in_data
-	data_length = len(data_set)
+	input(in_data)
 	
-	if (length <= 20):
-		for i in range(data_length):
-			for j in range(data_length):
-				if (j != i):
-					gcd(data_set[i], data_set[j])
-	
-	
+main(data_set)
